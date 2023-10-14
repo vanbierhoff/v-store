c5d50fa3e5eb51f0198c97386d31f66d58b2c665
hoff <ie@fv.ee>
2023-10-14 11:55:22 +0300

    updated version

--- projects/v/store/src/store/models/store-module.interface.ts
+++ projects/v/store/src/store/models/store-module.interface.ts
@@ -1,5 +1,4 @@
 import { StoreItemInterface } from '../store-items/store-item/models/store-item.interface';
-import { FieldManager } from '@v/short-store';
 
 
 export interface StoreModuleInterface {
