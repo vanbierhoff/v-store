4da36b9b56e4e4f7521c070869894892d1b2da91
hoff <ie@fv.ee>
2023-12-21 15:54:42 +0300

    update imports

--- projects/v/store/src/public-api.ts
+++ projects/v/store/src/public-api.ts
@@ -11,6 +11,12 @@ export * from './store/decorators/store-instance/store-instance.decorator';
 export * from './store/decorators/store-field/models/store.field-decorator.interface';
 export * from './store/decorators/store-instance/models/store-instance/store-instance.interface';
 
+// models store items
+export * from './store/store-items/models/store-events';
+export * from './store/store-items/store-field/models/store-field-meta';
+export * from './store/store-items/store-field/models/store-field-options.interface';
+
+
 
 
 // STORE SERVICES
@@ -35,8 +41,21 @@ export * from './helpers';
 export * from './store/models/store-module.interface';
 export * from './store/store-items/store-item/models/store-item.interface';
 export * from './store/store-items/store-item/models/store-strategy';
+export * from './store/models';
 
 // instances
 export * from './store/store-items/store-field/field-manager/field-manager';
 export * from './store/store-items/store-item/store-item';
 export * from './store/store.module';
+export * from './store/store-items/store-item/store-item';
+export * from './store/store-items/store-field/store-field-instance';
+
+// STRATEGIES
+export * from './store/store-items/strategies/base-store.strategy';
+export * from './store/store-items/strategies/decorated-store.strategy';
+export * from './store/store-items/strategies/primitive-store.strategy';
+
+
+// VALIDATORS
+export * from './store/validators';
+
---
+++ projects/v/store/src/store/validators/index.ts
@@ -0,0 +1,3 @@
+
+
+export  * from './type-validator'
